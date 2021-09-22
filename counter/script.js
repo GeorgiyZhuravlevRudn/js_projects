e7552596f1dc7a31020d84a2b5c2315aad224ac3
George <g_zhuravlev@g.zhuravlev>
2021-09-22 12:00:27 +0300

    commited

--- counter/script.js
+++ counter/script.js
@@ -1,5 +1,5 @@
 const number= document.querySelector(".number");
-let num=0;
+let num = 0;
 const increase=document.getElementById("increase");
 const decrease=document.getElementById("decrease");
 const reset=document.getElementById("reset");
@@ -7,12 +7,23 @@ const reset=document.getElementById("reset");
 increase.addEventListener("click",function(){
         num++;
         number.textContent=num;
+        checkNum();
 })
 decrease.addEventListener("click",function(){
         num--;
         number.textContent=num;
+        checkNum();
 })
 reset.addEventListener("click",function(){
         num=0;
         number.textContent=num;
+        checkNum();
 })
+function checkNum(){
+        if(num < 0)
+                number.style.color="red";
+        else if(num>0)
+                number.style.color="green";
+        else
+                number.style.color="black";
+}
