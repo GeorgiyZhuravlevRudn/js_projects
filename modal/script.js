5401d2321c0517bf938c6171860a08bc48af669c
George <g_zhuravlev@g.zhuravlev>
2021-09-23 19:47:57 +0300

    commited

---
+++ modal/script.js
@@ -0,0 +1,15 @@
+const modalOpen=document.querySelector(".modal-open");
+const modalClose = document.querySelector(".modal-close");
+
+const popUp = document.querySelector(".modal-overlay");
+console.log(popUp.classList);
+
+modalOpen.addEventListener('click', function() {
+        popUp.classList.add("open-modal");
+        console.log(popUp.classList);
+})
+
+modalClose.addEventListener('click', function() {
+        popUp.classList.remove("open-modal");
+        console.log(popUp.classList);
+})
